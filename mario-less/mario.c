7d5113a7396680a283b0a5ae55e08bd54b71ee42
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-20 13:42:27 +0000

    Fri, Jan 20, 2023, 1:42 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,19 +3,23 @@
 
 int main(void)
 {
-    int height,i,j,k ;
+    int height, i, j, k ;
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
-    for(i = 1 ; i <= height ; i++)
+    for (i = 1 ; i <= height ; i++)
     {
-        for(j = 1 ; j <= (height-i) ; j++)
-        printf(" ");
+        for (j = 1 ; j <= (height - i) ; j++)
+        {
+            printf(" ");
+        }
         for(k = 1 ; k <= i ; k++)
-        printf("#");
+        {
+            printf("#");
+        }
         printf("\n");
     }
 }
