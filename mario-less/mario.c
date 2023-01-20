e1fb61efc6cfb72acd09e64c08ca22893ada81a2
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-20 11:36:50 +0000

    Fri, Jan 20, 2023, 11:36 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height,stored ;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while( height < 1 || height > 8 );
+    stored = height;
+    printf("Stored: %i\n",stored);
+
+    for( i = 1 ; i <= height ; i++)
+    {
+        for( j = 1 ; j <= (height-i))
+    }
+
+
 }
