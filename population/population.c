d378cedaa85652ef11a4b6bedb030462509eb9c9
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-21 15:16:45 +0000

    Sat, Jan 21, 2023, 3:16 PM +00:00

--- population/population.c
+++ population/population.c
@@ -9,29 +9,31 @@ int main(void)
     do
     {
         start = get_int("Start: ");
-    } while(start<9);
+    }
+    while (start < 9);
 
     // TODO: Prompt for end size
     int end;
     do
     {
         end = get_int("End: ");
-    } while(end<start);
+    }
+    while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
-    int born , pass , stay , llamas , n=0;
-    born = start/3;
-    pass = start/4;
+    int born, pass, stay, llamas, n = 0;
+    born = start / 3;
+    pass = start / 4;
     stay = born - pass;
     llamas = start;
 
-    while(llamas<end)
+    while (llamas < end)
     {
         llamas = llamas + stay;
         n++;
-        born = llamas/3;
-        pass = llamas/4;
-        stay = born -pass;
+        born = llamas / 3;
+        pass = llamas / 4;
+        stay = born - pass;
     }
     // TODO: Print number of years
     printf("Number Of Years: %i \n", n);
