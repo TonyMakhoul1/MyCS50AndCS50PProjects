d4b69a571d7a3acf473db52c1e100e939c7116f1
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-26 16:18:43 +0000

    Thu, Jan 26, 2023, 6:18 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -25,6 +25,7 @@ int compute_score(string word)
 {
     // TODO: Compute and return score for string
     int score = 0;
+    
     for ( int i = 0; i < strlen(word); i++ )
     {
         socre += score;
