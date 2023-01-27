1c7d6f55a80ecea0a5bb96e0ffd2876c47a0c194
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-27 12:52:36 +0000

    Fri, Jan 27, 2023, 2:52 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int count_letters(string Text);
+int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     string text;
-    int letters;
+    int letters, words;
+
+
     text = get_string("Text: ");
+
+
     letters = count_letters(text);
     printf("letters: %d\n",letters);
+    words = count_words(text);
+    printf("words: %d\n",words);
+
+
+
 
     printf("Text: %s\n",text);
 
@@ -25,8 +35,17 @@ int count_letters(string text)
         {
             counter++;
         }
+    }
+    return counter;
+}
 
-
+int count_words(string text)
+{
+    int counter = 0;
+    for( int i = 0; i < strlen(text); i++ )
+    {
+        if( text[i] == " " )
+        counter++;
     }
     return counter;
 }
