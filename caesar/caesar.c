99a8aa0e5f8780f36eb038f1a24b837f785e0fae
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-01-28 11:53:22 +0000

    Sat, Jan 28, 2023, 1:53 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -44,7 +44,7 @@ bool only_digits(string s)
 {
     for( int i = 0; i < strlen(s); i++)
     {
-        if((!isdigit(s[i])) && (s[i] >= 0 && s[i] <= 9))
+        if((!isdigit(s[i])) )
         {
             printf("Usage: ./caesar key\n");
             return 1;
