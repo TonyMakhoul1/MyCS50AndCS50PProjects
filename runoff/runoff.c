4ed68a963fc162165da6bb951a8a6dc567fb7161
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-03 14:14:19 +0000

    Fri, Feb 3, 2023, 4:14 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -202,12 +202,12 @@ bool is_tie(int min)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min)
+        if ((candidates[i].votes != min) && (!candidates[i].eliminated))
         {
-            candidates[i].eliminated = true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
