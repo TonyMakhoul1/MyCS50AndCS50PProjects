47f00b1a41fc5a986848caf259a13cfdd52afee0
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-09 16:42:46 +0000

    Thu, Feb 9, 2023, 6:42 PM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,6 +40,10 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
+    while(fread(&header, size_of(int16_t), 1, input))
+    {
+        
+    }
 
     // Close files
     fclose(input);
