42426e3e646c4933440132f3fd1cac73d26ad2a8
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-10 16:18:35 +0000

    Fri, Feb 10, 2023, 6:18 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -18,6 +18,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE original[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+        }
+    }
     return;
 }
 
