c5562b9e3565cfb41e623e63bb37be22f6cc013a
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-10 19:38:26 +0000

    Fri, Feb 10, 2023, 9:38 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -28,7 +28,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0, int swapcol = width-1; j < width; j++)
+        for (int j = 0, swapcol = width-1; j < width; j++)
         {
             image[i][j] = original[i][swapcol];
         }
