381abca1a0dbaa94a2d4741e910b762cae9883e5
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-10 16:01:53 +0000

    Fri, Feb 10, 2023, 6:01 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,8 +7,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avg = round((image[height][width].rgbtRed +[height] image[width].rgbtGreen + image[height][width].rgbtBlue) / 3.0)
-            image[height][width].rgbtRed = [height] image[width].rgbtGreen = image[height][width].rgbtBlue = avg
+            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0)
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avg
         }
     }
     return;
