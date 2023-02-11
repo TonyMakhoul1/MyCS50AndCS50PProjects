ff777c60beb12ffe48f89b46edcf9ff2e7b34448
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-11 18:09:02 +0000

    Sat, Feb 11, 2023, 8:09 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -47,8 +47,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int count = 0;
-            int rowx[] = {i-1; i; i+1};
-            int colx[] = {j-1; j; j+1};
+            int rowx[] = {i-1, i, i+1};
+            int colx[] = {j-1, j, j+1};
             for (int row = 0; row < 3; row++)
             {
                 for(int col = 0; col < 3; col++)
