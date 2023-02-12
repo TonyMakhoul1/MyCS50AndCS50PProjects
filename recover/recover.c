9420b4ec4d15adc1d3db7df16aa1abe5fbed961a
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-12 17:36:53 +0000

    Sun, Feb 12, 2023, 7:36 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+typedef unint_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
     int count = 0;
     BYTE buffer[512];
     
+
 
 
 
