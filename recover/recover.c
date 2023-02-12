50640bab2f9d335ebfdff5070449ea5f678942e3
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-12 17:45:39 +0000

    Sun, Feb 12, 2023, 7:45 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     int count = 0;
     BYTE buffer[512];
 
-    while (fread(buffer, 1, 1, file) == 1)
+    while (fread(buffer, 512, 1, file) == 1)
     {
         
     }
