e0df6a9a54ff5dba8f7375b3be6db11c19d6af5a
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-12 16:36:09 +0000

    Sun, Feb 12, 2023, 6:36 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,15 +3,17 @@
 
 int main(int argc, char *argv[])
 {
+    FILE *file;
     if (argc != 2)
     {
         printf("Usage recover\n");
         return 1;
     }
-    if (FILE *file = fopen(argv[1], "r") == NULL)
+    if (file = fopen(argv[1], "r") == NULL)
     {
-        printf("Can't open %s", argv[1])
+        printf("Can't open %s", argv[1]);
     }
     
+
 
 }
