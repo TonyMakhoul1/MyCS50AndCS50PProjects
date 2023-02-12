a7faa882136713c083ff2d2bb73c31e3713eb365
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-12 18:18:28 +0000

    Sun, Feb 12, 2023, 8:18 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,6 +25,12 @@ int main(int argc, char *argv[])
     while (fread(buffer, 512, 1, file) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (!found)
+            {
+                
+            }
+        }
     }
 
 }
