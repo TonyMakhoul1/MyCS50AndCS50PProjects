52eb192140d7d36da85e2ca4756d8b64c9f646e7
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-16 19:11:55 +0000

    Thu, Feb 16, 2023, 9:11 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -82,13 +82,14 @@ void free_family(person *p)
     // TODO: Handle base case
     if (p == NULL)
     {
-        return 1;
+        return;
     }
 
     // TODO: Free parents recursively
-    
-
+    free(p -> parents[0]);
+    free(p -> parents[1]);
     // TODO: Free child
+    free(p);
 
 }
 
