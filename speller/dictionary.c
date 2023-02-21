936a745ef637777a132ccc23f14d85a2eb56dbf8
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-02-21 18:57:44 +0000

    Tue, Feb 21, 2023, 8:57 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -31,7 +31,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    
+
     return toupper(word[0]) - 'A';
 }
 
@@ -76,6 +76,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
+    
     return 0;
 }
 
