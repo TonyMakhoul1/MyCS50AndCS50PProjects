1e3c65f310f0304411381072cc59a1609911524f
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 11:28:27 +0000

    Fri, Mar 3, 2023, 1:28 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -57,7 +57,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    while 
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+        
 
 
 
