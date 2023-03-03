a4c9d5b1a0ea998a54d96fc42b167cfdb576d074
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 17:14:54 +0000

    Fri, Mar 3, 2023, 7:14 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,8 +1,10 @@
 # TODO
 from cs50 import get_int
 
-while height < 1 | height > 8:
+while True:
     height = get_int("Height: ")
+    if height >= 1 and height <= 8:
+        break
 
 for i in range(height):
     for j in range(height - i):
