016f2c963a359e89c4ef79e4180049e265323b77
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 09:19:09 +0000

    Fri, Mar 3, 2023, 11:19 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -20,7 +20,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = (LENGTH + 1) * 'z';
  int count = 0;
 
 // Hash table
