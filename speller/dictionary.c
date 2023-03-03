af52e29a358433efb2b236d8ca9fc454debade0b
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 08:26:20 +0000

    Fri, Mar 3, 2023, 10:26 AM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -19,6 +19,7 @@ node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
+const unsigned int count = 0;
 
 // Hash table
 node *table[N];
@@ -91,6 +92,8 @@ bool load(const char *dictionary)
         {
             n -> next = table[hashh];
         }
+        table[hashh] = n;
+        count +=1;
         fclose(file);
     }
 
