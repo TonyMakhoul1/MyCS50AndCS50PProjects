33bb489fd6b3fbf159d5b37ae7fcc7026ddfcec3
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 17:28:21 +0000

    Fri, Mar 3, 2023, 7:28 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,10 +5,8 @@ while True:
     height = get_int("Height: ")
     if height >= 1 and height <= 8:
         break
-i = 1
-j = 1
-k = 1
-for i  in range(height):
+
+for i in range(height):
     for j in range(height - i):
         print(" ", end = "")
     for k in range(i):
