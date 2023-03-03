db2450a0f35fcd27c7b343a7d545cbadb3d30001
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-03 11:04:27 +0000

    Fri, Mar 3, 2023, 1:04 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    file = open()
+    file = open("2018m.csv", "a")
+    
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
