cded9f34bf2d3a543e34a3de94c3d1c275590d13
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-04 13:33:04 +0000

    Sat, Mar 4, 2023, 3:33 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,4 +1,11 @@
 # TODO
 import cs50
+from cs50 import get_int
 def get_cents():
-    
+    while True:
+        cents = get_int("Cents: ")
+        if cents > 0:
+            break
+
+def calculate_quarters(cents):
+    q = cents / 25
