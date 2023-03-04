93fe3cedd1f1a0f2e2e51d802d2f65e98820a495
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-04 18:53:58 +0000

    Sat, Mar 4, 2023, 8:53 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,30 +3,7 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
-def count_letters(text):
-    counter = 0
-    for i in text:
-        if text[i].islower() | text[i].isupper:
-            counter += 1
-    return counter
 
-letters = count_letters(text)
-
-def count_words(text):
-    counter = 1
-    for i in text:
-        if text[i] == 32:
-            counter += 1
-    return counter
-words = count_words(text)
-
-def count_sentences(text):
-    counter = 0
-    for i in text:
-        if text[i] in {33, 46,63}:
-            counter += 1
-    return counter
-sentences = count_sentences(text)
 
 L = (letters / words) * 100
 S = (sentences / words) * 100
