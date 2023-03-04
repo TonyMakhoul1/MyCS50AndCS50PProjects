d900158545b70610e3eb185c4bc783f72fade1c0
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-04 13:52:41 +0000

    Sat, Mar 4, 2023, 3:52 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -22,3 +22,23 @@ def calculate_nickels(cents):
 def calculate_pennies(cents):
     p = cents / 1
     return p
+
+
+def main():
+    cents = get_cents()
+
+    quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
+
+    coins = quarters + dimes + nickels + pennies
+
+    print(coins)
