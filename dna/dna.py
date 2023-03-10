d1a5578f6b5c2bdf6cd4482e9622dc173d9eae94
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-10 17:42:42 +0000

    Fri, Mar 10, 2023, 7:42 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,18 +12,33 @@ def main():
 
     # TODO: Read database file into a variable
     database = []
-    with open("argv[1]", "r") as file:
+    with open(argv[1], "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             database.append(row)
 
 
     # TODO: Read DNA sequence file into a variable
+    with open(sys.argv[2],'r') as file:
+        dnaseq = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    subsequences = list(database[0].keys())[1:]
+    result = {}
+    for subsequence in subsequences:
+        result[subsequence] = longest_match(dnaseq, subsequence)
 
     # TODO: Check database for matching profiles
+    for person in database:
+        match = 0
+        for subsequence in subsequences:
+            if int(person[subsequence]) == result[subsequence]:
+                match += 1
 
+        if match == len(subsequences):
+            print(person["name"])
+            return
+        print("No Match")
     return
 
 
