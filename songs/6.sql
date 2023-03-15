93eb6de895c5611bb175e16dbe38d5ff2a5718ca
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 15:04:39 +0000

    Wed, Mar 15, 2023, 5:04 PM +02:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,1 @@
-SELECT name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = 'Post Malone';
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');
