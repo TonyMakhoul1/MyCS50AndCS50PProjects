7c75b693e518de9b8f06240fd4b61d6cf5c26b5a
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 17:16:27 +0000

    Wed, Mar 15, 2023, 7:16 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT(name) FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.title IN
-(SELECT DISTINCT(name) FROM people
+(SELECT DISTINCT(movies.title) FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE name = 'Kevin Bacon' and birth = '1958') AND name != 'Kevin Bacon';
