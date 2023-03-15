7b44599eacc66ec8f1671dc6f0ce39eecf7dfff9
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 19:55:07 +0000

    Wed, Mar 15, 2023, 9:55 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -48,3 +48,5 @@ WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id= (
 SELECT id FROM airports WHERE city = "Fiftyville")
 ORDER BY hour,minute
 LIMIT 1);
+
+SELECT phone_number FROM people WHERE name = "Bruce";
