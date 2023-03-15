b9f00fb8fa9e88393a5fa8cc0412c9ec8b23e8f7
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 19:03:00 +0000

    Wed, Mar 15, 2023, 9:03 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,3 +4,7 @@ WHERE year = 2021 AND day = 28 AND month = 7 AND street = 'Humphrey Street';
 
 SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+
+--Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+--If you have security footage from the bakery parking lot,
+--you might want to look for cars that left the parking lot in that time frame.
