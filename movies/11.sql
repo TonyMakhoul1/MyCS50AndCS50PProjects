b97e68692fe87472921727a99ddf64c7b0eaf61d
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 16:45:15 +0000

    Wed, Mar 15, 2023, 6:45 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC LIMIT 5;
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN people ON stars.person_id = people.id WHERE name = 'Chadwick Boseman' ORDER BY rating DESC LIMIT 5;
