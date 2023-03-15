649b9837095b8d3f391c7b4eee33bf658707453d
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 18:50:03 +0000

    Wed, Mar 15, 2023, 8:50 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+SELECT description FROM crime_scene_reports
+WHERE year = 2021 AND day = 28 AND month = 7 AND street = 'Humphrey Street';
