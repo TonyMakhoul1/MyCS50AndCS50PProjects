e73a54d69d1c22ff774e6e31a0ae511bf658388e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 19:54:04 +0000

    Wed, Mar 15, 2023, 9:54 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -40,3 +40,11 @@ LIMIT 1);
 SELECT name FROM people
 JOIN phone_calls ON phone_calls.caller = people.phone_number
 WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
+
+
+SELECT city FROM airports
+WHERE id = (SELECT destination_airport_id FROM flights
+WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id= (
+SELECT id FROM airports WHERE city = "Fiftyville")
+ORDER BY hour,minute
+LIMIT 1);
