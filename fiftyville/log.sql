55a6dacb18c466bee319ec9fefe97937c049441f
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 19:11:38 +0000

    Wed, Mar 15, 2023, 9:11 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,7 +8,10 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 --Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
 --If you have security footage from the bakery parking lot,
 --you might want to look for cars that left the parking lot in that time frame.
-
+SELECT name FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25
+AND activity = "exit";
 
 
 --I don't know the thief's name, but it was someone I recognized.
