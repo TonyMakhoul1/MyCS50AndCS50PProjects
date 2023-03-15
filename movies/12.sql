147f1328d2b90a694455ebe973b7cb38f3bc0c01
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-03-15 16:57:15 +0000

    Wed, Mar 15, 2023, 6:57 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = 'Johnny Depp' AND name = 'Helena Bonham Carter';
