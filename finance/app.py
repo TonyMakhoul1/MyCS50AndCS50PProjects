c53a5844ce4b5bb4cd48169bdfe5bfe8c7bfec86
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-16 17:14:24 +0000

    Sun, Apr 16, 2023, 8:14 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -214,10 +214,10 @@ def sell():
 
         user_shares = db.execute("SELECT shares FROM action WHERE id = ? AND symbol = ? GROUP BY symbol", user_id , symbol)
 
-        real_shares = user_shares[0]["shares"]
 
 
-        if shares > real_shares:
+
+        if shares > user_shares:
             return apology("You don't Have This Number Of Shares")
         update_cash = user_cash + action_value
 
