dd34ffcecc303b2b223abea4a780858690ffb4d2
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-16 16:53:24 +0000

    Sun, Apr 16, 2023, 7:53 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -212,10 +212,10 @@ def sell():
 
         user_cash = user_cash_db[0]["cash"]
 
-      //  user_shares = db.execute("SELECT shares FROM action WHERE id = ? AND symbol = ? GROUP BY symbol", user_id , symbol)
-       // real_shares = user_shares[0]["shares"]
-      //  if shares > real_shares:
-        //    return apology("You don't Have This Number Of Shares")
+        user_shares = db.execute("SELECT shares FROM action WHERE id = ? AND symbol = ? GROUP BY symbol", user_id , symbol)
+        real_shares = user_shares[0]["shares"]
+        if shares > real_shares:
+            return apology("You don't Have This Number Of Shares")
         update_cash = user_cash + action_value
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?", update_cash, user_id)
