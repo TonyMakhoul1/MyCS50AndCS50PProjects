9d6e3d59e4e90e0054d9fae406559264b5482ee3
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-19 16:29:44 +0000

    Wed, Apr 19, 2023, 7:29 PM +03:00

--- project/app.py
+++ project/app.py
@@ -1,7 +1,7 @@
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask("__name__")
 
