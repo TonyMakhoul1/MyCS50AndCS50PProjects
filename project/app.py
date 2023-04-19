71fa01d8d949861835dae306ce0c0c7b9b8e7499
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-19 10:10:29 +0000

    Wed, Apr 19, 2023, 1:10 PM +03:00

--- project/app.py
+++ project/app.py
@@ -3,3 +3,11 @@ from flask import Flask, redirect, render_tmepalte, request, session
 
 
 app = Flask("__name__")
+
+app.config["SESSION_PERMANT"] = false
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+app.route("/")
+def index:
+    
