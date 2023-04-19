97a0ad29c61f15a2c486ab6eced163b84731c5a1
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-19 13:59:37 +0000

    Wed, Apr 19, 2023, 4:59 PM +03:00

--- project/app.py
+++ project/app.py
@@ -21,3 +21,14 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
 
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmpassword = request.form.get("confirmpassword")
+        
+
+
