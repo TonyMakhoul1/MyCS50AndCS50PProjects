4ef0897e2f07ce99b6293fc07e58f7c208531e1e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-04-20 18:10:10 +0000

    Thu, Apr 20, 2023, 9:10 PM +03:00

--- project/app.py
+++ project/app.py
@@ -212,8 +212,8 @@ def sell():
 
         update_cash = user_cash + price_car
         quantity = quantity_car + 1
-        quantity_car_user_db = db.execute("SELECT * FROM user_car WHERE user_id = ? AND name_car = ?", user_id, name)
-        quantity_car_user = quantity_car_user_db[0]["quantity_car"]
+        #quantity_car_user_db = db.execute("SELECT * FROM user_car WHERE user_id = ? AND name_car = ?", user_id, name)
+        #quantity_car_user = quantity_car_user_db[0]["quantity_car"]
         if quantity_car_user <= 0:
             message4 = "You Don't Have This Car"
             return render_template("message.html", message = message4)
