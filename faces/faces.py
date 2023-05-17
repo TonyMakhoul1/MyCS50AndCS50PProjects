6bb21cb6f887ea400130e6b8850ce85b4b7b0251
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-17 15:24:31 +0000

    Wed, May 17, 2023, 6:24 PM +03:00

--- faces/faces.py
+++ faces/faces.py
@@ -3,4 +3,11 @@ def main():
 def convert():
     text = input("")
     x = text.find(":)")
-    
+    if x:
+        newx = x.replace(":")
+        print()
+    y = text.find(":(")
+    if y:
+        newy = y.replace("🙁")
+
+
