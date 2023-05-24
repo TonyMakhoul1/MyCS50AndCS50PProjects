12a6e7ac6e77affa0a19fc60f1819a7cba7e8e83
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 15:12:36 +0000

    Wed, May 24, 2023, 6:12 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,4 +1,5 @@
 message = input("File name: ")
+'''
 if ".jpg" or ".jpeg" in message:
     print("image/jpeg")
 elif ".gif" in message:
@@ -7,3 +8,15 @@ elif ".pdf" in message:
     print("application/pdf")
 else:
     print("application/octet-stream")
+'''
+match name:
+    case ".jpeg":
+        print("image/jpeg")
+    case ".jpg":
+        print("image/jpeg")
+    case ".gif":
+        print("image/gif")
+    case ".pdf":
+        print("application/pdf")
+    case _:
+        print("application/octet-stream")
