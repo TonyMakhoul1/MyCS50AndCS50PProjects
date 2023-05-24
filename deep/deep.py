11574137f423f24ee23dc8546b7fd1a794f1b1c0
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 10:20:26 +0000

    Wed, May 24, 2023, 1:20 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -6,7 +6,7 @@ def main():
 def yes_or_no(n):
         yes = "yes"
         no = "no"
-        if n == "42" or n == "forty-two" or n == "forty two" :
+        if n == "42" or n == "forty-two" or n == "forty two" or n == "FoRty TwO" or n == " 42 " :
             print("yes", end="")
             return
         else:
