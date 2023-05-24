d6663861a0aefabb8175906c6d3b9c2dedcd8dab
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 15:19:46 +0000

    Wed, May 24, 2023, 6:19 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,9 +3,17 @@ if ".jpg" in message:
     print("image/jpeg")
 elif ".jpeg" in message:
     print("image/jpeg")
+elif ".png" in message:
+    print("image/png")
 elif ".gif" in message:
     print("image/gif")
 elif ".pdf" in message:
+    print("application/pdf")
+elif ".txt" in message:
+    print("text/plain")
+elif ".zip" in message:
+    print("application.zip")
+elif ".PDF" in message:
     print("application/pdf")
 else:
     print("application/octet-stream")
