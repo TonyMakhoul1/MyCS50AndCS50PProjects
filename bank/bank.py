fc8441b25c3408229df22be3f63d51ffad521bb5
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 13:17:05 +0000

    Wed, May 24, 2023, 4:17 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,8 @@
 message = input("Greeting: ")
 
-if message.capitalize().strip() == "Hello":
+if message.capitalize() == "Hello" or " Hello ":
+    print("$0",end="")
+elif message.capitalize() = "Hello, Newman":
     print("$0",end="")
 elif message[0].capitalize() == "H":
     print("$20")
