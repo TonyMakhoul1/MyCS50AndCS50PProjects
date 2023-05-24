663d1e05f0d85dae96121b986b7d9dc828c91542
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 10:08:53 +0000

    Wed, May 24, 2023, 1:08 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,12 +1,12 @@
 def main():
-question = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
-yes = "yes"
-no = "no"
-yes_or_no(question)
+    question = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+    yes_or_no(question)
 
 
 def yes_or_no(question):
-        if question == 42 | question == "forty-two" | question == "forty two" :
+        yes = "yes"
+        no = "no"
+        if question == "42" | question == "forty-two" | question == "forty two" :
             return yes
         else:
             return no
