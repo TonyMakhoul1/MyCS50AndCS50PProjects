8882f1794dd412df9e7916da2c68282db147a94e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 15:38:51 +0000

    Wed, May 24, 2023, 6:38 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -4,3 +4,18 @@ x, y, z = expression.split(" ")
 if y == "+":
     result = x + z
     float(result)
+    round(result,1)
+elif y == "-":
+    result = x - z
+    float(result)
+    round(result,1)
+elif y == "/":
+    z != 0
+    result = x / z
+    float(result)
+    round(result,1)
+elif y == "%":
+    z != 0
+    result = x % z
+    float(result)
+    round(result,1)
