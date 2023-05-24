79d03feee20f1679ed103d0f7af0d7cd13a0c165
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 14:45:11 +0000

    Wed, May 24, 2023, 5:45 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,9 @@
 message = input("File name: ")
-if 
+if ".jpg" or "jpeg" in message:
+    print("image/jpeg")
+elif ".gif" in message:
+    print("image/gif")
+elif ".pdf" in message:
+    print("application/pdf")
+else:
+    print("application/octet-stream")
