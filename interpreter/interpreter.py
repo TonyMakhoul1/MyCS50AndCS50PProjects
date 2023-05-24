7f6bbd5dbb205c7b10b6b4dae321d197592b8c13
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 15:43:12 +0000

    Wed, May 24, 2023, 6:43 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,21 +1,28 @@
-expression = input("Expression: ")
-x, y, z = expression.split(" ")
+def main():
+    expression = input("Expression: ")
+    x, y, z = expression.split(" ")
 
-if y == "+":
-    result = x + z
-    float(result)
-    round(result,1)
-elif y == "-":
-    result = x - z
-    float(result)
-    round(result,1)
-elif y == "/":
-    z != 0
-    result = x / z
-    float(result)
-    round(result,1)
-elif y == "%":
-    z != 0
-    result = x % z
-    float(result)
-    round(result,1)
+    if y == "+":
+        result = x + z
+        float(result)
+        #round(result,1)
+        print(result)
+    elif y == "-":
+        result = x - z
+        float(result)
+        #round(result,1)
+        print(result)
+    elif y == "/":
+        z != 0
+        result = x / z
+        float(result)
+        #round(result,1)
+        print(result)
+    elif y == "%":
+        z != 0
+        result = x % z
+        float(result)
+        #round(result,1)
+        print(result)
+
+main()
