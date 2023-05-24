9611e03b736e7b9c602e88353f906319ca12e0cb
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-24 14:28:58 +0000

    Wed, May 24, 2023, 5:28 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,5 @@ if message.capitalize() == "Hello":
     print("$0",end="")
 elif message[0].capitalize() == "H":
     print("$20")
-elif message.capitalize() == "Hello, Newman":
-    print("$0",end="")
 else:
     print("$100")
