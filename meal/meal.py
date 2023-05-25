99cccf0a7258683a5a5099de3946c8b9e6f091e1
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-25 15:35:32 +0000

    Thu, May 25, 2023, 6:35 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,11 @@
 def main():
     time = input("What time is it? ")
     time = convert(time)
-    if time < 8.0 and time >= 7.0:
+    if time <= 8.0 and time >= 7.0:
         print("breakfast time")
     if time <= 13.0 and time >= 12.0:
         print("lunch time")
-    if time < 19.0 and time >= 18.0:
+    if time <= 19.0 and time >= 18.0:
         print("dinner time")
 
 
