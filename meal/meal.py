1d4a25ba246219805939bdb1466c2bbf0c08f33f
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-05-25 15:25:16 +0000

    Thu, May 25, 2023, 6:25 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,5 +1,7 @@
 def main():
     time = input("What time is it? ")
+    time = convert(time)
+    
 
 
 
@@ -7,22 +9,8 @@ def convert(time):
     hours, minutes = time.split(":")
     hours = float(hours)
     minutes = float(minutes)
-    if minutes == "60":
-        hours = hours + 1
-        minutes = 0
-    if hours == "7" and (minutes <= 59 or minutes >= 00):
-        print("breakfast time")
-    if hours == "8" and minutes == "00":
-        print("breakfast time")
-    if hours == "12" and (minutes <= 59 or minutes >= 00):
-        print("lunch time")
-    if hours == "13" and minutes == "00":
-        print("lunch time")
-    if hours == "18" and (minutes <= 59 or minutes >= 00):
-        print("dinner time")
-    if hours == "19" and minutes == "00":
-        print("dinner time")
-    
+    new_minutes = minutes / 60
+    return hours + minutes
 
 if __name__ == "__main__":
     main()
