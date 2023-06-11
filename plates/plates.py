7f408124665f50d07bdb7b0a94088bed2ed4f618
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-11 17:35:30 +0000

    Sun, Jun 11, 2023, 8:35 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,7 +9,7 @@ def main():
 def is_valid(s):
     if len(s) < 2 or len(s) > 6:
         return False
-    if s[0:2].isalpha() == False:
+    if s[0].isalpha() == False or s[1].isalpha() == False:
         return False
     i = 0
     while i < len(s):
