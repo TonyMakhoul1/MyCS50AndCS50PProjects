a33aaf08c0517aaf489f212bea9d78512f95d4e0
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-11 12:08:26 +0000

    Sun, Jun 11, 2023, 3:08 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,4 +3,6 @@ amount = 50
 while amount > 0:
     print("Amount Due: ", amount)
     insert = int(input("Insert Coin: "))
-    
+    if insert in [5, 10, 20]:
+        amount = amount - insert
+        
