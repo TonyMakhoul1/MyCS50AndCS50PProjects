f7d8264330e67b2348746193da23ff31f79e51b1
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-11 14:32:39 +0000

    Sun, Jun 11, 2023, 5:32 PM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,4 +2,6 @@ input = input("Input: ")
 vowel = ["A","E","I","O","U","a","e","i","o","u"]
 for i in input:
     if i in vowel:
-        print("")
+        print("", end="")
+    else:
+        print(i)
