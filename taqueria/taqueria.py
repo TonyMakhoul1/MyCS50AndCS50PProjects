891ac1b7496c2740566f1b5adf9e20a82f1c37ed
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-27 17:07:48 +0000

    Tue, Jun 27, 2023, 8:07 PM +03:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -18,7 +18,7 @@ while True:
     else:
         if item == "":
             quit()
-        elif item.lower() or item.upper() in dict:
+        elif item.upper() in dict:
             total = total + dict[item]
             print("Total: $",end="")
             print(total)
