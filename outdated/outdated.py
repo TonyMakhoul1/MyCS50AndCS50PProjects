365cd89f85cf915d85151a904400fb58cb127be6
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-29 17:09:07 +0000

    Thu, Jun 29, 2023, 8:09 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,15 +13,17 @@ months = [
     "December"
 ]
 date = input("Date: ")
-try:
-    month, day, year = date.split("/")
-    if(month <= 12 and month >= 1) and (day <= 31 and day >= 1):
-        break
-except:
+while True:
     try:
-        lamonth, laday, layear = date.split(" ")
-        for i in months:
-            if lamonth == months[i]:
-                lamonth = i + 1
-        if(month <= 12 and month >= 1) and (day <= 31 and day >= 1):
+        month, day, year = date.split("/")
+        if(int(month) <= 12 and int(month) >= 1) and (int(day) <= 31 and int(day) >= 1):
             break
+    except:
+        try:
+            lamonth, laday, layear = date.split(" ")
+            for i in months:
+                if lamonth == months[i]:
+                    lamonth = i + 1
+
+            if(lamonth <= 12 and lamonth >= 1) and (laday <= 31 and laday >= 1):
+                break
