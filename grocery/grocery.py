be5394f5a45bc99cf58cf46acd3f19f31d6ef77e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-29 12:17:08 +0000

    Thu, Jun 29, 2023, 3:17 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,5 +1,13 @@
+dict = {}
 while True:
     try:
-        item = input()
+        item = input().lower()
+        if item in dict:
+            dict[item] = dict[item] + 1
+        else:
+            dict[item] = 1
     except EOFError:
-        
+        for item in dict:
+            print(dict[item], item.upper())
+            break
+
