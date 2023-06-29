3fb56381202a1716b70cfeccde5b70eaf224dc95
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-29 17:06:04 +0000

    Thu, Jun 29, 2023, 8:06 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,4 +19,9 @@ try:
         break
 except:
     try:
-        lamonth, laday, layear = date.split("")
+        lamonth, laday, layear = date.split(" ")
+        for i in months:
+            if lamonth == months[i]:
+                lamonth = i + 1
+        if(month <= 12 and month >= 1) and (day <= 31 and day >= 1):
+            break
