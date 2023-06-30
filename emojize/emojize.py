8abf7ee0979f91a4aca07978c7773e0dfffe1ccd
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-06-30 13:55:42 +0000

    Fri, Jun 30, 2023, 4:55 PM +03:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,8 +1,7 @@
 import emoji
-list = emoji
-text = input("Input: ")
-text.replace("_","")
 
-for i in list:
-    if list[i] == text:
-        print("Output: " + list[i])
+text = input("Input: ")
+
+output = emoji.emojize(text)
+
+print("Output: " + output)
