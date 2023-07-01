34d0139ff5b97dd1e6581f774d55cca37cc68db3
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-01 16:36:29 +0000

    Sat, Jul 1, 2023, 7:36 PM +03:00

--- game/game.py
+++ game/game.py
@@ -2,7 +2,17 @@ import random
 while True:
     try:
         level = input("Level: ")
-        level = random.randint(0,100)
+        #level = random.randint(0,100)
+        guess = input("Guess: ")
+        if guess < level:
+            print("Too small!")
+            break
+        elif guess > level:
+            print("Too large!")
+            break
+        else:
+            print("Just right!")
+            break
     except:
         print()
         exit()
