760e73a25939233bac7713e90d051abdd080f5f1
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-01 15:54:48 +0000

    Sat, Jul 1, 2023, 6:54 PM +03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,14 +1,3 @@
-# JOIN WORDS INTO A LIST:
-
-#mylist = p.join(("apple", "banana", "carrot"))
-# "apple, banana, and carrot"
-
-#mylist = p.join(("apple", "banana"))
-# "apple and banana"
-
-#mylist = p.join(("apple", "banana", "carrot"), final_sep="")
-# "apple, banana and carrot"
-
 import inflect
 p = inflect.engine()
 list = []
