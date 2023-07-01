6502d7e41cd5fad0b78f46f5f0e61c231e015ffe
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-01 15:16:11 +0000

    Sat, Jul 1, 2023, 6:16 PM +03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,10 +1,11 @@
 # JOIN WORDS INTO A LIST:
 
-mylist = p.join(("apple", "banana", "carrot"))
+#mylist = p.join(("apple", "banana", "carrot"))
 # "apple, banana, and carrot"
 
-mylist = p.join(("apple", "banana"))
+#mylist = p.join(("apple", "banana"))
 # "apple and banana"
 
-mylist = p.join(("apple", "banana", "carrot"), final_sep="")
+#mylist = p.join(("apple", "banana", "carrot"), final_sep="")
 # "apple, banana and carrot"
+
