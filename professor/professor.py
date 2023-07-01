594543812124dc1d37ce3eeba0195f5954cad311
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-01 17:44:50 +0000

    Sat, Jul 1, 2023, 8:44 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,18 +2,23 @@ import random
 
 
 def main():
-    ...
+    get_level()
 
 
 def get_level():
-while True:
-    try:
-        
+    while True:
+        try:
+            level = input("Level: ")
+            if level <= 3 or level >= 1:
+                break
+        except:
+            pass
+    return level
 
 
 
-def generate_integer(level):
-    ...
+#def generate_integer(level):
+
 
 
 if __name__ == "__main__":
