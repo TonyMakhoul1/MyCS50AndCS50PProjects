e21ab296edbbf40b341792b278a15ab460740ca5
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-01 16:59:17 +0000

    Sat, Jul 1, 2023, 7:59 PM +03:00

--- game/game.py
+++ game/game.py
@@ -3,26 +3,21 @@ while True:
     try:
         level = int(input("Level: "))
         #level = random.randint(0,100)
+
+    except:
+        pass
+
+number = 
+while True:
+    try:
         guess = int(input("Guess: "))
-        if guess < level:
-            print("Too small!")
-            break
-        elif guess > level:
-            print("Too large!")
-            break
-        else:
-            print("Just right!")
-            break
-    except TypeError:
-        level = int(input("Level: "))
-        #level = random.randint(0,100)
-        guess = int(input("Guess: "))
-        if guess < level:
-            print("Too small!")
-            break
-        elif guess > level:
-            print("Too large!")
-            break
-        else:
-            print("Just right!")
-            break
+        if guess > 0:
+            if guess < level:
+                print("Too small!")
+                break
+            elif guess > level:
+                print("Too large!")
+                break
+            else:
+                print("Just right!")
+                break
