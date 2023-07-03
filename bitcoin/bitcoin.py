6798d1d54ac1e03634f161152f4720ed52f6f796
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-03 12:17:51 +0000

    Mon, Jul 3, 2023, 3:17 PM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,6 +4,15 @@ import requests
 if len(sys.argv) == 2:
     try:
         number = float(sys.argv[1])
+        try:
+            r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            result = r.json()
+            amount = result['bpi']['USD']['rate_float']
+            final_amount = amount * number
+            print(f"${final_amount:,.4f}")
+        except requests.RequestException:
+            print("Request Exception")
+            sys.exit(1)
     except:
         print("Command-line argument is not a number")
         exit(1)
@@ -11,10 +20,3 @@ else:
     print("Missing command-line argument")
     sys.exit(1)
 
-try:
-    r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    result = r.json()
-    print(result[])
-except requests.RequestException:
-    print("Request Exception")
-    sys.exit(1)
