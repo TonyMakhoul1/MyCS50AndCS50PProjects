cec4b4731d2694f367c770b41f5af9fbb4124af4
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-04 16:13:26 +0000

    Tue, Jul 4, 2023, 7:13 PM +03:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,7 +3,40 @@ def main():
 
 
 def convert(fraction):
-    ...
+    try:
+        f = fraction
+        x,y = f.split("/")
+        x = int(x)
+        y = int(y)
+    except ValueError:
+        f = fraction
+        x,y = f.split("/")
+        x = int(x)
+        y = int(y)
+
+    except ZeroDivisionError:
+        f = fraction
+        x,y = f.split("/")
+        x = int(x)
+        y = int(y)
+    else:
+        if x == 0:
+            print("E")
+        elif x < y or x == y:
+            z = (x / y) * 100
+            z = round(z)
+            if z <= 1:
+                print("E")
+            elif z >= 99:
+                print("F")
+            else:
+                print(z,end ="%")
+                print()
+        elif x > y:
+            fraction = input("Fraction: ")
+            x,y = fraction.split("/")
+            x = int(x)
+            y = int(y)
 
 
 def gauge(percentage):
