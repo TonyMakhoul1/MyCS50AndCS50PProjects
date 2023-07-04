211676867abaa42480aa09836b1a72c9a662d948
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-04 09:42:20 +0000

    Tue, Jul 4, 2023, 12:42 PM +03:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -11,10 +11,8 @@ print()
 def main():
 
 def shorten(word):
-    input = input("Input: ")
     vowel = ["A","E","I","O","U","a","e","i","o","u"]
-    print("Output: ", end = "")
-    for i in input:
+    for i in word:
         if i in vowel:
             print("", end="")
         else:
