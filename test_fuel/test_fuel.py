06623ea6390bba37f00419cb7556ad189ae434fa
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-04 16:55:24 +0000

    Tue, Jul 4, 2023, 7:55 PM +03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -3,3 +3,12 @@ import pytest
 
 def test_one():
     assert convert("2/4") == 50 and gauge(50) == "50%"
+
+def test_two():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_three():
+    with pytest.raises(ValueError):
+        convert("Dog/Cat")
+        
