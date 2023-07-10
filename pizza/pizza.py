efe4fd11307975111ca2e1489f7c6f2d43718ea7
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-10 09:18:30 +0000

    Mon, Jul 10, 2023, 12:18 PM +03:00

---
+++ pizza/pizza.py
@@ -0,0 +1,10 @@
+def check_command():
+    if len(sys.argv) < 2 :
+        print("Too few arguments")
+        sys.exit(1)
+    if len(sys.argv) > 2 :
+        print("Too many arguments")
+        sys.exit(1)
+    if ".csv" not in sys.argv[1] :
+        print("Not a CSV file")
+        sys.exit(1)
