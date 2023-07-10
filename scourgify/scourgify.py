8f9e8ef53a0e27914fab898f705f67e1132a8e8e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-10 11:05:20 +0000

    Mon, Jul 10, 2023, 2:05 PM +03:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+    check_command
+    try:
+
+    except FileNotFoundError:
+        print("File does not exist")
+        sys.exit(1)
+
+def check_command():
+    if len(sys.argv) < 3 :
+        print("Too few arguments")
+        sys.exit(1)
+    if len(sys.argv) > 3 :
+        print("Too many arguments")
+        sys.exit(1)
+    if ".csv" not in sys.argv[1] :
+        print("Not a CSV file")
+        sys.exit(1)
