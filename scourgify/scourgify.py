bd86c8c9b9fa05b287a119240482dd2fafb679b8
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-10 11:52:31 +0000

    Mon, Jul 10, 2023, 2:52 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -17,7 +17,7 @@ def main():
             writer = csv.writer(cs)
             for s in list:
                 writer.writerow({"name":s["name"], "house":s["house"]})
-                
+
     except FileNotFoundError:
         print(f"Could not read {sys.argv[1]}")
         sys.exit(1)
@@ -29,7 +29,7 @@ def check_command():
     if len(sys.argv) > 3 :
         print("Too many arguments")
         sys.exit(1)
-    if ".csv" not in sys.argv[1] :
+    if ".csv" not in (sys.argv[1] or sys.argv[2]) :
         print("Not a CSV file")
         sys.exit(1)
 
