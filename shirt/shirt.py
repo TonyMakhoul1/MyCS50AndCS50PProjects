89401bc59b7dd1efd9b27cf101b844926952c8b7
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-11 10:07:47 +0000

    Tue, Jul 11, 2023, 1:07 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,8 +1,14 @@
 import sys
+import os
 
 def main():
     check_command()
     print("hello")
+    try:
+
+    except FileNotFoundError:
+        print("Input does not exist")
+        sys.exit(1)
 
 def check_command():
     if len(sys.argv) < 3 :
@@ -11,7 +17,19 @@ def check_command():
     if len(sys.argv) > 3 :
         print("Too many arguments")
         sys.exit(1)
-    
+    file1 = os.path.splitext(sys.argv[1])
+    file2 = os.path.splitext(sys.argv[2])
+    if file1[1] not in [".jpeg", ".jpg",".png"]:
+        print("Invalid output")
+        sys.exit(1)
+    if file2[1] not in [".jpeg", ".jpg",".png"]:
+        print("Invalid output")
+        sys.exit(1)
+
+    if file1[1] != file2[1]:
+        print("Input and output have different extensions")
+        sys.exit(1)
+
 
 
 if __name__ == "__main__":
