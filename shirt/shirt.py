ccc063a0bdc82c0418a28b4d8dbffe13d9a63a12
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-11 08:08:51 +0000

    Tue, Jul 11, 2023, 11:08 AM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -10,8 +10,8 @@ def check_command():
     if len(sys.argv) > 3 :
         print("Too many arguments")
         sys.exit(1)
-    if (".jpeg" or "jpg" or "png") not in (sys.argv[1] or sys.argv[2]) :
-        print("Not a CSV file")
+    if (".jpeg" or ".jpg" or ".png") not in (sys.argv[1] or sys.argv[2]) :
+        print("names do not end with jpeg, jpg or png")
         sys.exit(1)
 
 
