ada3930ec9a2e9ba893795842ea07fbe47363ad8
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-11 10:01:29 +0000

    Tue, Jul 11, 2023, 1:01 PM +03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -11,9 +11,7 @@ def check_command():
     if len(sys.argv) > 3 :
         print("Too many arguments")
         sys.exit(1)
-    if (".jpeg" or ".jpg" or ".png") not in (sys.argv[1] or sys.argv[2]) :
-        print("names do not end with jpeg, jpg or png")
-        sys.exit(1)
+    
 
 
 if __name__ == "__main__":
