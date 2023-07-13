3e0e0bbe3f95a00b404f9c92faee5c31c0ddcde3
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-13 17:16:34 +0000

    Thu, Jul 13, 2023, 8:16 PM +03:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,4 +1,10 @@
 from um import count
 
 def test_one():
-    assert 
+    assert count("um") == 1
+
+def test_two():
+    assert count("tummy") == 0
+
+def test_three():
+    assert count("tu um im um") == 2
