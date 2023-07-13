9bc2fa2bcb3cc7cfd44039aa322eb98bb769a6fa
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-13 15:42:15 +0000

    Thu, Jul 13, 2023, 6:42 PM +03:00

--- working/test_working.py
+++ working/test_working.py
@@ -2,3 +2,7 @@ from working import convert, not_am_pm
 
 def test_one():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+
+def test_two():
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    
