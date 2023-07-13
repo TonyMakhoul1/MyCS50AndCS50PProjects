fe0ca51cc03682b9ded41300cb472024e4370813
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-07-13 11:21:28 +0000

    Thu, Jul 13, 2023, 2:21 PM +03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    if re.search("^([0-9]+\.){3}[0-9]+$", ip):
+    if re.search(r"^([0-9]+\.){3}[0-9]+$", ip):
         list = ip.split(".")
         for s in list:
             if int(s) > 255 or int(s) < 0:
--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,7 +1,7 @@
 from numb3rs import validate
 
 def testone():
-    assert validate(124.5.6.4) == True
+    assert validate("124.5.6.4") == True
 
 def testtwo():
-    assert validate(255.300.255.255) == False
+    assert validate("255.300.255.255") == False
