a29e37ecc8a0d535aae6b438dc4e7031ba24f009
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-14 11:25:13 +0000

    Mon, Aug 14, 2023, 2:25 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,7 +1,9 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
-
+        if capacity < 0:
+            raise ValueError("Can't be a negative number")
+        self.capacity = capacity
+        
     def __str__(self):
         ...
 
