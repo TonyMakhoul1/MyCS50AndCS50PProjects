22d48b0d3d9c2468600b58782ee347407ae9043e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-14 12:31:38 +0000

    Mon, Aug 14, 2023, 3:31 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,23 +2,25 @@ class Jar:
     def __init__(self, capacity=12):
         if capacity < 0:
             raise ValueError("Can't be a negative number")
+        self._capacity = capacity
+        self._size = 0
 
     def __str__(self):
-        return "🍪" * self.size
+        return "🍪" * self._size
 
     def deposit(self, n):
-        if self.size + n > self.capacity:
+        if self._size + n > self._capacity:
             raise ValueError("Too Much")
         if n > self.capacity:
             raise ValueError("Too Much")
-        self.size = self.size + n
+        self._size = self._size + n
 
     def withdraw(self, n):
         if n < 0:
             raise ValueError("Negative Number")
-        if self.size - n < 0:
+        if self._size - n < 0:
             raise ValueError("We Don't Have Enough Cookies")
-        self.size = self.size - n
+        self._size = self._size - n
     @property
     def capacity(self):
         return self._capacity
@@ -28,4 +30,6 @@ class Jar:
         return self._size
 
 jar = Jar()
+jar.deposit(3)
+jar.withdraw(2)
 print(jar)
