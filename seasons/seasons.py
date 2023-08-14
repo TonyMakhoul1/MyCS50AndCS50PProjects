a3dac53c44c87c2567f99e77bf3dcd560e38150e
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-14 10:11:44 +0000

    Mon, Aug 14, 2023, 1:11 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,10 +5,11 @@ import sys
 def main():
     birth_date = input("Date of Birth: ")
     try:
-        check_birth(birth_date)
-        print
+        year, month, day = check_birth(birth_date)
+        print(year, month, day)
+
     except:
-        sys.exit(1)
+        sys.exit("Invalid Date")
 
 def check_birth(birth):
     if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", birth):
