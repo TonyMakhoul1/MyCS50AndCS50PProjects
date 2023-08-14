6279fdb4aab8478f2f7e5d65d26bb08163b7bd71
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-14 11:01:28 +0000

    Mon, Aug 14, 2023, 2:01 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,9 +1,8 @@
 from seasons import check_birth
-import pytest
+
 
 def test_one():
     assert check_birth("2002-09-09") == ("2002", "09", "09")
 
 def test_two():
-    with pytest.raises(ValueError):
-        check_birth("2002-2-2")
+    assert check_birth("2002-9-9") == None
