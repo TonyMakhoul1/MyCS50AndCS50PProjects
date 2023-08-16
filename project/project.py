507913b23525aa6e56e45b41226d5a42b80a6d24
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-16 16:17:10 +0000

    Wed, Aug 16, 2023, 7:17 PM +03:00

--- project/project.py
+++ project/project.py
@@ -1,6 +1,8 @@
 import sys
 def main():
-    check_cmd()
+    
+        check_cmd()
+
 
 
 def check_cmd():
@@ -10,7 +12,7 @@ def check_cmd():
     if len(sys.argv) > 3:
         print("Too many arguments")
         sys.exit(1)
-    if ".csv" not in (argv[1] and argv[2]):
+    if ".csv" not in (sys.argv[1] or sys.argv[2]):
         print("Not csv file")
         sys.exit(1)
 
