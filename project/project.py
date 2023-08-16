92a7f62695111b1057475873e6028732bd9064eb
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-16 17:50:09 +0000

    Wed, Aug 16, 2023, 8:50 PM +03:00

--- project/project.py
+++ project/project.py
@@ -32,8 +32,9 @@ def main():
                         reader = csv.DictReader(file)
                         for row in reader:
                             list.append(row)
-                        #for row in list:
-                            #  print(row["Name"])
+                        for row 
+
+                        print(list)
 
                 except FileNotFoundError:
                     print(f"Could not read {sys.argv[1]}")
