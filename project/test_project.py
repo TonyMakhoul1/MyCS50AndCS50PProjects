f92dafcd29ca99f208e54b7c29e76b32100d6ab2
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-17 15:21:10 +0000

    Thu, Aug 17, 2023, 6:21 PM +03:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,11 +1,19 @@
 from project import check_calories, check_price, check_cmd
-def test_function_1():
-    ...
+import pytest
+def test_check_cmd():
+    with pytest.raises(SystemExit):
+        check_cmd()
 
 
-def test_function_2():
-    ...
+def test_check_calories():
+    assert check_calories(100) == "To all weight"
+    assert check_calories(250) == "To 120 Kg"
+    assert check_calories(350) == "To 100 Kg"
+    assert check_calories(500) == "To 80 Kg"
 
 
-def test_function_n():
-    ...
+
+def test_check_price():
+    assert check_price(10) == "Low cost"
+    assert check_price(20) == "Well cost"
+    assert check_price(30) == "High cost"
