b2f84cec92b9af6fba62d54ea1d6c69616193353
TonyMakhoul1 <122486302+TonyMakhoul1@users.noreply.github.com>
2023-08-17 23:52:41 +0000

    Fri, Aug 18, 2023, 2:52 AM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -12,8 +12,9 @@ months = [
     "November",
     "December"
 ]
-date = input("Date: ")
+
 while True:
+    date = input("Date: ")
     try:
         month, day, year = date.split("/")
         if(int(month) <= 12 and int(month) >= 1) and (int(day) <= 31 and int(day) >= 1):
